ef18a8ab7101d98ff9e94fb842fb69a8207006be
Diogo Dias <1161605@isep.ipp.pt>
2021-11-11 16:58:30 +0000

    LAP22G31-3 #comment Final UI and Controller to funtionalities

--- src/main/java/lapr/project/ui/MainUI.java
+++ src/main/java/lapr/project/ui/MainUI.java
@@ -5,34 +5,39 @@ import lapr.project.controller.*;
 public class MainUI {
 
 
-    private Object code;
-    private Object date;
-    private Object date1;
-    private Object date2;
-    private Object n;
+    private String code1="211331640";
+    private String code2= "DHBN";
+    private String code3="IMO9193305";
+    private String date="31/12/2020 17:19";
+    private String date2="30/01/2021 17:19";
+    private int n=5;
 
     public void run() {
 
         MainController mainController = new MainController();
+        //Pedro
         mainController.importFile(); //US101 - Import file & US103 - Positional Messages
 
-        mainController.searchDetails(code); //US102 - Search Ship by code MMSI
-        mainController.searchDetails(code); //US102 - Search Ship by code CallSign
-        mainController.searchDetails(code); //US102 - Search Ship by code ISO
+        mainController.searchDetails(code1); //US102 - Search Ship by code MMSI
+        mainController.searchDetails(code2); //US102 - Search Ship by code CallSign
+        mainController.searchDetails(code3); //US102 - Search Ship by code ISO
 
-        mainController.searchDate(code,date); //US103 - Search Ship by code MMSI on Date
-        mainController.searchDate(code,date); //US103 - Search Ship by code CallSign on Date
-        mainController.searchDate(code,date); //US103 - Search Ship by code ISO on Date
+        //Diogo
+        mainController.searchDate(code1,date); //US103 - Search Ship by code MMSI on Date
+        mainController.searchDate(code2,date); //US103 - Search Ship by code CallSign on Date
+        mainController.searchDate(code3,date); //US103 - Search Ship by code ISO on Date
 
-        mainController.searchDate(code,date1,date2); //US103 - Search Ship by code MMSI on Date Range
-        mainController.searchDate(code,date1,date2); //US103 - Search Ship by code CallSign on Date Range
-        mainController.searchDate(code,date1,date2); //US103 - Search Ship by code ISO on Date Range
+        mainController.searchDate(code1,date,date2); //US103 - Search Ship by code MMSI on Date Range
+        mainController.searchDate(code2,date,date2); //US103 - Search Ship by code CallSign on Date Range
+        mainController.searchDate(code3,date,date2); //US103 - Search Ship by code ISO on Date Range
 
-        mainController.summary(code); //US104 - Summary Ship by code MMSI
-        mainController.summary(code); //US104 - Summary Ship by code CallSign
-        mainController.summary(code); //US104 - Summary Ship by code ISO
+        //Duarte
+        mainController.summary(code1); //US104 - Summary Ship by code MMSI
+        mainController.summary(code2); //US104 - Summary Ship by code CallSign
+        mainController.summary(code3); //US104 - Summary Ship by code ISO
 
-        mainController.getTopN(n,date1,date2); //US106 - TopN for Date Range and Group By Vessel Type
+        //Diogo
+        mainController.getTopN(n,date,date2); //US106 - TopN for Date Range and Group By Vessel Type
 
         mainController.pairsofShips(); //US107 - Pair Ships with close location and Diferent Travel Distance
 
