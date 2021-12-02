80d36b6e82411e216ff9c64efda07026bb38c7ae
Diogo Dias <1161605@isep.ipp.pt>
2021-12-02 12:29:21 +0000

    LAP22G31-94 #comment Database Testing

---
+++ src/test/java/lapr/project/model/DatabaseTest.java
@@ -0,0 +1,34 @@
+package lapr.project.model;
+
+import lapr.project.data.DatabaseConnection;
+import org.junit.jupiter.api.Test;
+
+
+
+
+import java.sql.Connection;
+
+public class DatabaseTest {
+
+    /***
+     * Test closest port?
+     */
+    @Test
+    public void testClosestPortDatabase(){
+        /*
+        DatabaseConnection databaseConnection = mock(DatabaseConnection.class);
+
+        Connection connection = mock(Connection.class);
+        
+         */
+    }
+
+    /***
+     * Client wants to Know Situation of Container
+     */
+    @Test
+    public void testContainer(){
+
+    }
+
+}
